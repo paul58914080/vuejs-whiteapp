94d58ea8d96ec0dd45621a717872a29058b72225
Paul <paul58914080@gmail.com>
2018-04-30 12:25:12 +0530

    separating into different files (html, ts and scss)

---
+++ src/components/helloworld/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template src="./HelloWorld.html">
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss" src="./helloworld.scss">
+
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,13 @@
+<template src="./home.html">
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '../../components/helloworld/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
+</script>
